ff3659fc9fb04cad8e62f794bb65fe33daa76f52
Karn Yongsiriwit <karn.y@rsu.ac.th>
2021-05-23 21:33:36 +0700

    remove AccountCircle

--- src/Profile.js
+++ src/Profile.js
@@ -4,7 +4,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import Avatar from '@material-ui/core/Avatar';
